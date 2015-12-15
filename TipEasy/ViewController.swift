de7489842af386c0105952f2389ed3d9afa5bd9b
jpecoraro342 <jpecoraro342@ufl.edu>
2015-12-15 17:30:26 -0500

    Finished following video

--- TipEasy/ViewController.swift
+++ TipEasy/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var billAmountField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00";
+        totalLabel.text = "0.00";
     }
-
+    
+    @IBAction func onEditChanged(sender: AnyObject) {
+        let tipPercentages = [0.18, 0.2, 0.22];
+        
+        let billAmount = NSString(string: billAmountField.text!).doubleValue;
+        let selectedTip = tipPercentages[tipControl.selectedSegmentIndex];
+        let tipAmount = selectedTip * billAmount;
+        
+        let total = billAmount + tipAmount;
+        
+        tipLabel.text = getCurrencyFormat(Float(tipAmount));
+        totalLabel.text = getCurrencyFormat(Float(total));
+    }
+    
+    func getCurrencyFormat(amount: Float) -> String {
+        return String(format: "$%.2f", amount);
+    }
+    
+    @IBAction func backgroundTapped(sender: AnyObject) {
+        self.view.endEditing(true);
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
